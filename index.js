a9e2f3462895fa1dbfbf08f5830afda9582387a4
ian <iandavis6481@gmail.com>
2023-06-24 09:43:57 -0500

    prisma init config

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Choo Choo! Welcome to your Express app 🚅');
-})
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Choo Choo! Welcome to your Express app 🚅");
+});
 
 app.get("/json", (req, res) => {
-    res.json({"Choo Choo": "Welcome to your Express app 🚅"});
-})
+  res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
+});
+
+app.get("/todos", async (req, res) => {
+  const todos = await prisma.todo.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+
+  res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+  const todo = await prisma.todo.create({
+    data: {
+      completed: false,
+      createdAt: new Date(),
+      text: req.body.text ?? "Empty todo",
+    },
+  });
+
+  return res.json(todo);
+  //   return res.json(req.body.text);
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
